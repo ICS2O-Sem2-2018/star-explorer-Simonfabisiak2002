bcb16e9bd13fc8c40a84de5af360da385e80b400
Simonfabisiak2002 <simon.fabisiak@ocsbstudent.ca>
2018-03-28 13:35:47 -0400

    did a lot of changes i think idk
    did it sorta i guess sorta im not sure

--- main.lua
+++ main.lua
@@ -4,4 +4,13 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local composer = require( "composer" )
+
+
+display.setStatusBar( display.HiddenStatusBar )
+
+
+math.randomseed( os.time() )
+
+
+composer.gotoScene( "menu" )
---
+++ menu.lua
@@ -0,0 +1,99 @@
+
+local composer = require( "composer" )
+
+local scene = composer.newScene()
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+
+local function goToGame()
+		composer.gotoScene( "game" )
+end
+
+local function goToHighScores()
+		composer.goToScene( "highscores" )
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+
+	local sceneGroup = self.view
+	-- Code here runs when the scene is first created but has not yet appeared on screen
+
+	local background = display.newImageRect( sceneGroup, "background.png", 2000, 2400 )
+	background.x = displaycontentCenterX
+	background.y = displaycontentCenterY
+
+	local title = display.newImageRect( sceneGroup, "HL3.png", 690, 420 )
+	title.x = 375
+	title.y = 500
+
+	local playButton = display.newText( sceneGroup, "SPOOKY SKELETON SAYS PLAY", display.contentCenterX, 700, native.systemFont, 30 )
+	playButton:setFillColor( 0.82, 0.86, 1)
+
+	local highScoresButton = display.newText( sceneGroup, "THE BEST OF THE BEST", display.contentCenterX, 810, native.systemFont, 44 )
+	highScoresButton:setFillColor( 0.78, 0.79, 1 )
+
+	playButton:addEventListener( "tap", goToGame )
+	highScoresButton:addEventListener( "tap", goToHighScores)
+end
+
+
+-- show()
+function scene:show( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+
+	end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene
